dbdf5cd1baf350b178f2763836203fd11ce61f4c
be-great <begreat5333@gmail.com>
2024-08-08 22:31:20 +0300

    update task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+    $('input[type="checkbox"]').change(function() {
+        // get from the checkbox the attr or variable
+        const amenityid = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        // if a input is checked add it to the dictonary
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityid] = amenityName;
+        }
+        else {
+            delete selectedAmenities[amenityid];
+        }
+        // update the h4 tag inside the div Amenities with the list of Amenities checked
+        const list = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(list);
+    });
+});
