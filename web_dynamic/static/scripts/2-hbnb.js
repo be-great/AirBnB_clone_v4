bd04d8faeffd78d35c43abaa36e817dc4af5448b
be-great <begreat5333@gmail.com>
2024-08-10 17:23:59 +0300

    update task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
     });
     $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
 
-        if (data.status == 'OK')
+        if (data.status == "OK")
         {
-            $('#api_status').addClass('available');
+            $('div#api_status').addClass('available');
         }
         else {
-            $('#api_status').removeClass('available');
+            $('div#api_status').removeClass('available');
         }
     }).fail(function() {
-        $('#api_status').removeClass('available');
+        $('div#api_status').removeClass('available');
     });
 });
